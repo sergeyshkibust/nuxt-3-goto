dbe0261c8e992940912ef5fe91887d4b9d3666e0
Sergey Shkibust <sergey.shkibusts@gmail.com>
2023-07-28 13:42:09 +0300

    Update v2

--- src/providers/linkProvider.ts
+++ src/providers/linkProvider.ts
@@ -21,7 +21,9 @@ export default class LinkProvider implements vsDocumentLinkProvider {
 
         if (config.quickJump) {
             let exception = 'a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|bgsound|big|blink|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|command|content|data|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|element|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|image|img|input|ins|isindex|kbd|keygen|label|legend|li|link|listing|main|map|mark|marquee|menu|menuitem|meta|meter|multicol|nav|nobr|noembed|noframes|noscript|object|ol|optgroup|option|output|p|param|plaintext|pre|progress|q|rp|rt|rtc|ruby|s|samp|script|section|select|shadow|slot|small|source|spacer|span|strike|strong|style|sub|summary|sup|table|tbody|td|template|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp';
-            let reg = new RegExp(`<((?!\/?(${exception})).+?)(\\s+[^>]*?)?\/?>`, 'g');
+            //<((?!\/?(${exception})).+?)(\\s+[^]*?)?>
+            //<((?!\/?(${exception})))(\w*)\s?[^>][:\w="\?\.\s\'-\[\]\{\}]*\/?[>\n]
+            let reg = new RegExp(`<(?!\/)(?!(${exception}))(\\w*)\\s?[:\\w="\\?\\.\\s\\'-\\[\\]\\{\\}]*\/?[>]?[\\n]?`, 'g');
             let linesCount = doc.lineCount <= config.maxLinesCount ? doc.lineCount : config.maxLinesCount;
             let index = 0;
             const workspaceFolder = workspace.getWorkspaceFolder(doc.uri);
@@ -34,8 +36,9 @@ export default class LinkProvider implements vsDocumentLinkProvider {
                 let line = doc.lineAt(index);
                 let match;
                 while ((match = reg.exec(line.text)) !== null) {
-                    let tagName = match[1];
-                    if (!tagName.startsWith('/')) {
+                    let test = match;
+                    let tagName = match[2];
+                    if (tagName !== '') {
                         tags.push({ tag: tagName, line: line });
                     }
                 }
